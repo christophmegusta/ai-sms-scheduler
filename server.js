61e80eb89efeeafc0202569f10ff646fe2b8fb67
Chris <christoph@megusta.ag>
2023-04-20 00:32:25 +0200

    WIP added solana wallet authentication

--- server.js
+++ server.js
@@ -2,64 +2,90 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { SIWS } = require('@web3auth/sign-in-with-solana');
+const jwt = require('jsonwebtoken');
 
 const { addScheduledMessage, getScheduledMessages, saveScheduledMessage, deleteScheduledMessage } = require("./smsScheduler");
 
+const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
 
 const port = process.env.PORT || 3000;
+
+const users = {};
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
+function verifyToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  console.log("authHeader", authHeader);
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
 
-async function isValidSignature(signature) {
-  // TODO: Implement signature validation logic using solana web3 signatures
-  // https://solana-labs.github.io/solana-web3.js/classes/Signature.html
-  // ...
-  return true;
-  
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.user = user;
+      console.log("allowed user", user);
+      next();
+    });
+  } else {
+    console.log("denied user");
+    res.sendStatus(401);
+  }
 }
 
-app.get("/messages", async (req, res) => {
-  if(!isValidSignature(req.headers["signature"])) {
-    res.status(401).send("Invalid signature");
-    return;
-  }
 
+app.post('/verifySolana', async (req, res) => {
+  try {
+    const { header, payload, signature } = req.body;
+
+    const msg = new SIWS({ header, payload });
+    const resp = await msg.verify({ payload, signature });
+
+    if (resp.success) {
+      const publicKey = payload.address;
+
+      // Check if the user exists, if not, create a new user
+      if (!users[publicKey]) {
+        users[publicKey] = { publicKey };
+      }
+
+      // Generate a JWT for the user
+      const token = jwt.sign({ publicKey }, JWT_SECRET, { expiresIn: '1h' });
+
+      res.json({ status: 'success', message: 'Verified', token });
+    } else {
+      res.status(400).json({ status: 'error', message: 'Verification failed' });
+    }
+  } catch (error) {
+    res.status(500).json({ status: 'error', message: error.message });
+  }
+});
+
+app.get("/messages", verifyToken, async (req, res) => {
+  console.log("req.headers", req.headers);
   const messages = await getScheduledMessages();
   res.send(messages);
 });
 
-app.post("/schedule", async (req, res) => {
-  if(!isValidSignature(req.headers["signature"])) {
-    res.status(401).send("Invalid signature");
-    return;
-  }
-
+app.post("/schedule", verifyToken, async (req, res) => {
   const { phone, message, sendAt, recurrence, sendChance, timeWindow } = req.body;
   await addScheduledMessage(phone, message, sendAt, recurrence, sendChance, timeWindow);
   res.status(201).send(`Scheduled message added for ${phone} at ${sendAt}`);
 });
 
-app.post("/saveScheduledMessage", async (req, res) => {
-  if(!isValidSignature(req.headers["signature"])) {
-    res.status(401).send("Invalid signature");
-    return;
-  }
-
+app.post("/saveScheduledMessage", verifyToken, async (req, res) => {
   const { id, phone, message, sendAt, recurrence, sendChance, timeWindow } = req.body;
   await saveScheduledMessage(id, phone, message, sendAt, recurrence, sendChance, timeWindow);
   res.status(201).send(`Scheduled message saved for ${id} ${phone} at ${sendAt}`);
 });
 
-app.post("/deleteScheduledMessage", async (req, res) => {
-  if(!isValidSignature(req.headers["signature"])) {
-    res.status(401).send("Invalid signature");
-    return;
-  }
-
+app.post("/deleteScheduledMessage", verifyToken, async (req, res) => {
   const { id } = req.body;
   await deleteScheduledMessage(id);
   res.status(201).send(`Scheduled message deleted for ${id}`);
